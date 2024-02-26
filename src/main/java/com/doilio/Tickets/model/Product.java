fbceb816f594ec271584bc67848fe3fa56df6825
Doilio Matsinhe <doiliomatsinhe@gmail.com>
2024-02-26 15:23:23 +0200

    Add dependency for JPA Refactor code Store some information on PostgreSQL

--- src/main/java/com/doilio/Tickets/model/Product.java
+++ src/main/java/com/doilio/Tickets/model/Product.java
@@ -1,12 +1,62 @@
 package com.doilio.Tickets.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
-public record Product(
-        int id,
-        int eventId,
-        String name,
-        String descriprion,
-        BigDecimal price
-) {
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private int eventId;
+    @Column(nullable = false)
+    private String name;
+    @Column
+    private String descriprion;
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescriprion() {
+        return descriprion;
+    }
+
+    public void setDescriprion(String descriprion) {
+        this.descriprion = descriprion;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
